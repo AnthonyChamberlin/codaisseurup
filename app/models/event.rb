b88f711bd8e48739d879d1eda1af4a5ad12079d3
AnthonyChamberlin <antchamberlin.js@gmail.com>
2017-10-11 17:27:26 +0200

    Add Model Specs: Events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :user
 
   has_and_belongs_to_many :categories
+
+  validates :name,        presence: true, length: { maximum: 100 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :location,    presence: true
 end
